10f0755514d57f0c01b019a04a6edc06172a763a
ACT丶流星雨 <1340145680@qq.com>
2026-05-31 18:54:03 +0800

    完善工具调用

--- src/socket copy/routes/productionAgent.ts
+++ src/socket copy/routes/productionAgent.ts
@@ -1,10 +1,8 @@
-import { GlobalContext } from "@/socket/type";
 import jwt from "jsonwebtoken";
 import u from "@/utils";
 import { Namespace, Socket } from "socket.io";
 import { ChatMessagesData } from "@/socket/chatMessagesData";
 import * as agent from "@/agents/productionAgent/index";
-import ChatKit from "@/socket/chatKit";
 
 async function verifyToken(rawToken: string): Promise<Boolean> {
   const setting = await u.db("o_setting").where("key", "tokenKey").select("value").first();
@@ -20,35 +18,23 @@ async function verifyToken(rawToken: string): Promise<Boolean> {
   }
 }
 
+interface GlobalContext {
+  messages: ChatMessagesData[];
+}
+
 export default (nsp: Namespace) => {
   nsp.on("connection", async (socket: Socket) => {
     console.log("[productionAgent] 已连接:", socket.id);
 
     const globalContext: GlobalContext = {
-      remoteTools: [],
-      abortSignal: new AbortController(),
-      socket: socket,
-      kit: undefined as any,
-      isolationKey: "123",
-      projectId: 1777638289380,
-      thinkLevel: 0,
       messages: [],
     };
 
-    globalContext.kit = new ChatKit(globalContext.messages, {
-      onChange: (messages: ChatMessagesData[]) => socket.emit("syncMessages", messages),
-    });
+    socket.on("syncMessages", (messages: ChatMessagesData[]) => (globalContext.messages = messages));
 
-    socket.on("remoteTools", async (remoteTools) => (globalContext.remoteTools = remoteTools));
+    socket.on("chat", async (data: { content: string }) => {
+      agent.runDecisionAI(globalContext)
 
-    socket.on("syncMessages", (messages: ChatMessagesData[]) => {
-      globalContext.messages = messages;
-    });
-
-    socket.on("chat", async (text: string) => {
-      const userBox = globalContext.kit.user();
-      userBox.text(text).end();
-      agent.runDecisionAI(globalContext, text);
     });
   });
   nsp.on("disconnect", (socket: Socket) => {
